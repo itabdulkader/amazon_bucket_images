9d9c3d0ce93513ab41503a8380808d0f9625c4d0
itabdulkader <it.abdulkader@gmail.com>
2022-08-25 12:19:16 +0200

    first status

---
+++ app/Http/Controllers/ImageController.php
@@ -0,0 +1,25 @@
+<?php
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Storage;
+
+class ImageController extends Controller
+{
+    public function create()
+    {
+        return view('uploadimage');
+    }
+    public function imageUpload(Request $request)
+    {
+        $this->validate($request, ['image' => 'required|image']);
+        if($request->hasfile('image'))
+         {
+            $file = $request->file('image');
+            $name=time().$file->getClientOriginalName();
+            $filePath = 'images/' . $name;
+            Storage::disk('s3')->put($filePath, file_get_contents($file));
+            return back()->with('success','Image Uploaded successfully');
+         }
+    }
+}
